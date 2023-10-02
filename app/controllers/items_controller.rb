edd6e9c5a739e2832c5338f19e2db0bede37cc76
20230824 <nike52book@gmail.com>
2023-10-02 20:05:33 +0900

    販売手数料と販売利益、表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,16 @@
 class ItemsController < ApplicationController
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
