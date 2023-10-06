cd88e452b966be9f90f3a68d508cae5ba03fdd3c
20230824 <nike52book@gmail.com>
2023-10-06 10:27:11 +0900

    テストコード

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -4,7 +4,7 @@ class OrderShipping
 
   with_options presence: true do
     validates :post, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 } 
     validates :village
     validates :explanation
     validates :telephone, format: { with: /\A\d{10,11}\z/}
